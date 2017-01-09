dc8e63f4e1fdd901d7a9b5afcd948bada7a33bbf
Dante Conley <danteconley@icloud.com>
2017-01-09 16:52:11 -0700

    Imagew of ME

--- about-dante.php
+++ about-dante.php
@@ -3,10 +3,17 @@
 		<title>About Me</title>
 	</head>
 	<body>
-		<p>Name: Dante D Conley<br>
+		<p>
+		<b><u>ABOUT ME:</u></b><br><br>
+		Name: Dante D Conley<br>
 		Age: 24<br>
 		Location: Albuquerque NM<br><br>
+			<b><u>HOBBIES:</u></b></p><ul>
+		<li>Skateboarding<br></li>
+		<li>Skiing<br></li>
+		<li>Music Production<br></li>
+		<li>DJing<br><br></li>
+		</ul>
 		<img src="images/dante.JPG"/>
-		</p>
 	</body>
 </html>
